db2b3a733cb3de02411ca0021176bd4921895d7f
emobossemo <408455428@qq.com>
2017-08-14 17:33:10 +0800

    20170814

--- SmartSchool/src/app/config/menu.ts
+++ SmartSchool/src/app/config/menu.ts
@@ -2,90 +2,104 @@ export const Menu = [{
 	"id": "basic",
 	"name": "基础信息管理",
 	"children": [{
-		"icon":"class",
-		"name":"个人中心",
-		"url":"/app/basic/personal",
-		"description": "建设中..."
+			"icon":"class",
+			"name":"个人中心",
+			"url":"/app/basic/personal",
+			"description": "建设中..."
+		},{
+			"icon":"school",
+			"name":"学校管理",
+			"url":"/app/basic/schools",
+			"description": "建设中..."
+		},{
+			"icon":"school",
+			"name":"教职工信息",
+			"url":"/app/basic/staffs",
+			"description": "建设中..."
+		},{
+			"icon":"school",
+			"name":"账户管理",
+			"url":"/app/basic/account",
+			"description": "建设中..."
+		},{
+			"icon":"school",
+			"name":"部门管理",
+			"url":"/app/basic/departs",
+			"description": "建设中..."
+		},{
+			"icon":"subject",
+			"name":"教学楼管理",
+			"url":"/app/basic/buildings",
+			"description": "建设中..."
+		},{
+			"icon":"class",
+			"name":"教室办公室管理",
+			"url":"/app/basic/offices",
+			"description": "建设中..."
+		},{
+			"icon":"subject",
+			"name":"学科管理",
+			"url":"/app/basic/subjects",
+			"description": "建设中..."
+		},{
+			"icon":"subject",
+			"name":"课程管理",
+			"url":"/app/basic/course",
+			"description": "建设中..."
+		},{
+			"icon":"class",
+			"name":"年级管理",
+			"url":"/app/basic/grades",
+			"description": "建设中..."
+		},{
+			"icon":"class",
+			"name":"班级管理",
+			"url":"/app/basic/classes",
+			"description": "建设中..."
+		},{
+			"icon":"class",
+			"name":"密码管理",
+			"url":"/app/basic/password",
+			"description": "建设中..."
+		}]
 	},{
-		"icon":"school",
-		"name":"学校管理",
-		"url":"/app/basic/schools",
-		"description": "建设中..."
+		"id": "resource",
+		"name": "资源管理",
+		"children": [{
+			"icon":"school",
+			"name":"智慧云网盘",
+			"url":"/app/resources/index",
+			"description": "建设中..."
+		},{
+			"icon":"school",
+			"name":"我的资源",
+			"url":"/app/resources/mine",
+			"description": "建设中..."
+		},{
+			"icon":"school",
+			"name":"资源审核",
+			"url":"/app/resources/review",
+			"description": "建设中..."
+		}]
 	},{
-		"icon":"school",
-		"name":"教职工信息",
-		"url":"/app/basic/staffs",
-		"description": "建设中..."
+		"id": "resource",
+		"name": "资产管理",
+		"children": [{
+			"icon":"school",
+			"name":"资产管理",
+			"url":"/app/assets/index",
+			"description": "建设中..."
+		}]
 	},{
-		"icon":"school",
-		"name":"账户管理",
-		"url":"/app/basic/account",
-		"description": "建设中..."
-	},{
-		"icon":"school",
-		"name":"部门管理",
-		"url":"/app/basic/departs",
-		"description": "建设中..."
-	},{
-		"icon":"subject",
-		"name":"教学楼管理",
-		"url":"/app/basic/buildings",
-		"description": "建设中..."
-	},{
-		"icon":"class",
-		"name":"教室办公室管理",
-		"url":"/app/basic/offices",
-		"description": "建设中..."
-	},{
-		"icon":"subject",
-		"name":"学科管理",
-		"url":"/app/basic/subjects",
-		"description": "建设中..."
-	},{
-		"icon":"subject",
-		"name":"课程管理",
-		"url":"/app/basic/course",
-		"description": "建设中..."
-	},{
-		"icon":"class",
-		"name":"年级管理",
-		"url":"/app/basic/grades",
-		"description": "建设中..."
-	},{
-		"icon":"class",
-		"name":"班级管理",
-		"url":"/app/basic/classes",
-		"description": "建设中..."
-	}]
-},{
-	"id": "resource",
-	"name": "资源管理",
-	"children": [{
-		"icon":"school",
-		"name":"智慧云网盘",
-		"url":"/app/resources/index",
-		"description": "建设中..."
-	},{
-		"icon":"school",
-		"name":"我的资源",
-		"url":"/app/resources/mine",
-		"description": "建设中..."
-	},{
-		"icon":"school",
-		"name":"资源审核",
-		"url":"/app/resources/review",
-		"description": "建设中..."
-	}]
-},{
-	"id": "resource",
-	"name": "资产管理",
-	"children": [{
-		"icon":"school",
-		"name":"资产管理",
-		"url":"/app/resources/assets",
-		"description": "建设中..."
-	}]
-}
+		"id": "shedule",
+		"name": "排课系统",
+		"children": [{
+			"icon":"school",
+			"name":"走班排课",
+			"url":"",
+			"description": "建设中..."
+		}]
+	}
 
 ]
 
@@ -98,7 +112,12 @@ export const MenuTeacher = [{
 		"name":"个人中心",
 		"url":"/app/basic/personal",
 		"description": "建设中..."
-	}]
+	},{
+			"icon":"class",
+			"name":"密码管理",
+			"url":"/app/basic/password",
+			"description": "建设中..."
+		}]
 },{
 	"id": "resource",
 	"name": "资源管理",
@@ -128,7 +147,12 @@ export const MenuSuper = [{
 		"name":"学校管理",
 		"url":"/app/basic/schools",
 		"description": "建设中..."
-	}]
+	},{
+			"icon":"class",
+			"name":"密码管理",
+			"url":"/app/basic/password",
+			"description": "建设中..."
+		}]
 },{
 	"id": "resource",
 	"name": "资源管理",
@@ -154,9 +178,18 @@ export const MenuSuper = [{
 	"children": [{
 		"icon":"school",
 		"name":"资产管理",
-		"url":"/app/resources/assets",
+		"url":"/app/assets/index",
 		"description": "建设中..."
 	}]
-}
+},{
+		"id": "shedule",
+		"name": "排课系统",
+		"children": [{
+			"icon":"school",
+			"name":"走班排课",
+			"url":"",
+			"description": "建设中..."
+		}]
+	}
 
 ]
